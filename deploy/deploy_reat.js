79608b642ae8f124c1f7d7da7f657602509c328b
EnriqueGS88 <enrique.gzs@gmail.com>
2022-03-11 16:45:41 +0100

    extended deploy script to automatically call tx's immediately after deployment

--- deploy/deploy_reat.js
+++ deploy/deploy_reat.js
@@ -1,4 +1,5 @@
-const { network } = require('hardhat');
+const { hexStripZeros } = require('ethers/lib/utils');
+const { network, ethers } = require('hardhat');
 let { networkConfig } = require('../helper-hardhat-config');
 
 module.exports = async( {
@@ -12,6 +13,7 @@ module.exports = async( {
 
     log("Deploying 'MasterREAT_EG' Token Generator Contract");
     
+    // Deploy contract
     const MasterREAT = await deploy( 'MasterREAT_EG', {
         from: deployer,
         args: [],
@@ -22,7 +24,25 @@ module.exports = async( {
 
     const networkName = networkConfig[chainId]["name"];
 
+    // Print the following string to easily verify your contract
     log(`Verify your contract with: \n npx hardhat verify --network ${networkName} ${MasterREAT.address}`  );
+
+    // Get accounts and signers to consequently send transactions post deployment
+    const accounts = await hre.ethers.getSigners();
+    const signer = accounts[0];
+
+
+    // Below a set of tx's that will be sent to the newly deployed contract
+
+    // Get Contract instance to start calling its functions and sending transactions
+
+    const MasterREATContract = await ethers.getContractFactory('MasterREAT_EG');
+    const masterReat = new ethers.Contract(MasterREAT.address, MasterREATContract.interface, signer);
+
+    let _tokenId = 1;
+    let uri_tx = await masterReat.getUri( _tokenId );
+    log( "This is the tokenURI for the Id you provided: ", uri_tx );
+    
 
 };
 
